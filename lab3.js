41c2a278ac4eafc1eeec6a9e41658253edf3ffea
guna1743 <80980438+guna1743@users.noreply.github.com>
2021-10-30 23:58:45 -0700

    Update lab3.js

--- lab3.js
+++ lab3.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 const PORT= process.env.PORT||5700;
  var server= http.createServer(function(req,res){
     res.writeHead(200,{'Content-type':'text/html'});
-    fs.readFile('resume.html',null,function(error,data){
+    fs.readFile('index.html',null,function(error,data){
         if(error){
             res.writeHead(404);
             res.write('File not Found!')
